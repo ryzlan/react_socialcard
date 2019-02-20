dfeedf82f7b83d447d1c564035247a2255127d2a
Ruzlan Karim <ruzlankarim2@gmail.com>
2019-02-20 18:22:23 +0600

    Updates

---
+++ precache-manifest.07ff845cd14affcfaf18bc0be5a2f2b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b09c76ba781cf5b090c4",
+    "url": "/react_socialcard/static/js/runtime~main.b09c76ba.js"
+  },
+  {
+    "revision": "a920f2a2edf2dc186a4c",
+    "url": "/react_socialcard/static/js/main.a920f2a2.chunk.js"
+  },
+  {
+    "revision": "9d51dda1dc174f663998",
+    "url": "/react_socialcard/static/js/2.9d51dda1.chunk.js"
+  },
+  {
+    "revision": "a920f2a2edf2dc186a4c",
+    "url": "/react_socialcard/static/css/main.596ce70e.chunk.css"
+  },
+  {
+    "revision": "28dc038e957565d0f02b5029eafd9faa",
+    "url": "/react_socialcard/index.html"
+  }
+];
